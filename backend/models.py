0824b49ec61cb5a7af573e5d29fe231b3ab81090
Paul <20096590@mail.wit.ie>
2025-11-19 21:14:49 +0000

    Added users model and created users table for authentication

--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Column, Integer, Date, Numeric, String
+from sqlalchemy import Column, Integer, Date, Numeric, String, DateTime
+from sqlalchemy.sql import func
+
 from .db import Base
 
 
@@ -9,3 +11,16 @@ class Transaction(Base):
     date = Column(Date, nullable=False)
     description = Column(String, nullable=False)
     amount = Column(Numeric(12, 2), nullable=False)
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, nullable=False, unique=True, index=True)
+    password_hash = Column(String, nullable=False)
+    created_at = Column(
+        DateTime(timezone=True),
+        nullable=False,
+        server_default=func.now(),
+    )
